736ff847f982707079b4bf56a1101bfda2185afb
lillyclarke <114373455+lillyclarke@users.noreply.github.com>
2022-10-12 14:05:52 -0700

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,7 @@
+//const  = document.querySelector("");
+
+//cookie clicker
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  console.log("Hello world!");
+}) 
