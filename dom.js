ce100a611efb6b57f31b15235457b69be70fdf49
sal fierro <frrslo@yahoo.com>
2022-10-12 14:23:54 -0700

    lets hope this works

--- dom.js
+++ dom.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 // this is just to see if it works
-=======
 //const  = document.querySelector("");
 
 //cookie clicker
@@ -8,5 +6,4 @@ const button = document.querySelector("button");
 button.addEventListener("click", function () {
   console.log("Hello world!");
 }) 
->>>>>>> 736ff847f982707079b4bf56a1101bfda2185afb
 //this is a stage
